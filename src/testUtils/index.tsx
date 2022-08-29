cf85037dcf20dcd6f98a85cd0707fff80f8a00dd
Eric Brogin <ebrogin@synack.com>
2022-08-29 11:34:52 -0500

    main: Removed tests from eslint. Added test helpers.

---
+++ src/testUtils/index.tsx
@@ -0,0 +1,52 @@
+/* eslint-disable @typescript-eslint/ban-types */
+import React, { PropsWithChildren } from 'react'
+import { render, renderHook } from '@testing-library/react'
+import type { RenderOptions } from '@testing-library/react'
+import { combineReducers, configureStore } from '@reduxjs/toolkit'
+import type { PreloadedState } from '@reduxjs/toolkit'
+import { Provider } from 'react-redux'
+import type { RootState } from '../store/types'
+import onboardingsSliceReducer from '../features/onboarding/onboardingsSlice'
+import dataSliceReducer from '../features/data/dataSlice'
+
+const testState = { onboardings: {}, data: {} } as RootState
+const rootReducer = combineReducers({
+  onboardings: onboardingsSliceReducer,
+  data: dataSliceReducer,
+})
+const testStore = (state: RootState) => {
+  return configureStore({ reducer: rootReducer, preloadedState: state })
+}
+
+interface ExtendedRenderOptions extends Omit<RenderOptions, 'queries'> {
+  preloadedState?: PreloadedState<RootState>
+  store?: ReturnType<typeof testStore>
+}
+
+export function renderWithProviders(
+  ui: React.ReactElement,
+  {
+    preloadedState = testState,
+    // Automatically create a store instance if no store was passed in
+    store = testStore(testState),
+    ...renderOptions
+  }: ExtendedRenderOptions = {},
+) {
+  function Wrapper({ children }: PropsWithChildren<any>): JSX.Element {
+    return <Provider store={store}>{children}</Provider>
+  }
+
+  // Return an object with the store and all of RTL's query functions
+  return { store, ...render(ui, { wrapper: Wrapper, ...renderOptions }) }
+}
+
+export function renderHookWithProvider(
+  hook: Function,
+  store = configureStore({ reducer: rootReducer }),
+) {
+  function Wrapper({ children }: PropsWithChildren<any>): JSX.Element {
+    return <Provider store={store}>{children}</Provider>
+  }
+
+  return renderHook(() => hook(), { wrapper: Wrapper })
+}
